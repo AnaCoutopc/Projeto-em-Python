db3a46126f6d9a1f1e0bf9c98da2b21987a71fba
Ana Paula Couto <86626863+AnaCoutopc@users.noreply.github.com>
2022-03-25 17:26:34 -0300

    Soma e multiplicação
    Soma e multiplicação de quatro números

---
+++ Soma e multiplicação de quatro números.py
@@ -0,0 +1,8 @@
+#Projeto de soma e multiplicação de quatros números#
+n1 = int(input('digite um número'))
+n2 = int(input('digite um número'))
+n3 = int(input('digite um número'))
+n4 = int(input('digite um número'))
+S = n1 + n2 + n3 + n4
+M = n1 * n2 * n3 * n4
+print(S , M)
