60ef6aad26eb8c4653909b556d83865c016248cd
Ana Paula Couto <86626863+AnaCoutopc@users.noreply.github.com>
2022-03-25 17:29:50 -0300

    Média da notas
    Nesse projeto fazemos o cálculo da média dos alunos.

---
+++ Média de notas.py
@@ -0,0 +1,8 @@
+#Média de notas#
+a = float(input("Digite a nota: "))
+b = float(input("Digite a nota: "))
+c = float(input("Digite a nota: "))
+d = float(input("Digite a nota: "))
+media = float
+media = (a + b + c + d) / 4
+print(media)
