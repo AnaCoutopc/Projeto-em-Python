3b43aa46858175fb4469d308745e3f4dc6422e60
Ana Paula Couto <ana.paula.coutopc@gmail.com>
2022-03-27 02:04:38 -0300

    Create Projeto que cálcula o IMC.py
    Esse projeto calcula o IMC

---
+++ Projeto que cálcula o IMC.py
@@ -0,0 +1,9 @@
+#Projeto que cálcula o IMC#
+h=float(input('Qual é a sua altura?'))
+homens=float
+mulheres=float
+homens = (72.7 * h) - 58
+print ('peso ideal dos homens é {:.2f}'.format(homens))
+mulheres = (62.1 * h) - 44.7
+print('peso ideal das mulheres {:.2f}'.format(mulheres))
+
