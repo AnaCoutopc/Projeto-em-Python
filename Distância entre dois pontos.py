05882e0c65a7ef68846abca33af481f44f8f5451
Ana Paula Couto <ana.paula.coutopc@gmail.com>
2022-04-05 14:31:04 -0300

    Distância entre pontos

---
+++ Distância entre dois pontos.py
@@ -0,0 +1,8 @@
+#Distância entre dois pontos#
+distancia = int(input())
+velocidade_km_hora = int(input())
+tempo_hora = float
+velocidade_segundos = int
+tempo_hora = distancia / velocidade_km_hora
+velocidade_segundos = (distancia / tempo_hora) / 3.6
+print ('O tempo de deslocamento {:.1f}h'.format(tempo_hora),'e a velocidade é: {:.1f}m/s'.format(velocidade_segundos))
